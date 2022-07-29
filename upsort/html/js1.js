70bc4ba31ca385bc6dfeb3fe48536134d0da2140
kwynncom <buessresume@gmail.com>
2022-07-29 01:31:01 -0400

    and a bit better

--- upsort/html/js1.js
+++ upsort/html/js1.js
@@ -1,11 +1,12 @@
 onDOMLoad(() => {
+
+    const df = { year    : 'numeric', month  : 'short'  , day    : '2-digit',
+                 weekday : 'short'  , hour   : '2-digit', minute : '2-digit', timeZoneName : 'short'};
+
     qsa('[data-u]').forEach((e) => {
         const U = e.dataset.u;
         const dateo = new Date(U * 1000);
-        
-        const df = { weekday: 'short', 'year': 'numeric', 'month' : 'short', 'day' : '2-digit', 'hour' : '2-digit', 'minute' : '2-digit', timeZoneName : 'short'};
-        
-        let s = dateo.toLocaleDateString([], df);
+        const s = dateo.toLocaleDateString([], df);
         inht(e, s);
     });
 });
