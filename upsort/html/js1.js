73480283a724aa665cfa771ba33d2939237b2485
kwynncom <buessresume@gmail.com>
2022-07-29 01:28:03 -0400

    correction - now they work

--- upsort/html/js1.js
+++ upsort/html/js1.js
@@ -3,7 +3,7 @@ onDOMLoad(() => {
         const U = e.dataset.u;
         const dateo = new Date(U * 1000);
         
-        const df = { weekday: 'short', 'year': '2-digit', 'month' : 'short', 'hour' : '2-digit', 'minute' : '2-digit', timeZoneName : 'short'};
+        const df = { weekday: 'short', 'year': 'numeric', 'month' : 'short', 'day' : '2-digit', 'hour' : '2-digit', 'minute' : '2-digit', timeZoneName : 'short'};
         
         let s = dateo.toLocaleDateString([], df);
         inht(e, s);
