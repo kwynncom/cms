503000934765719f0e535c7e9a10e82fd31eb8d5
kwynncom <buessresume@gmail.com>
2022-08-01 01:49:50 -0400

    working example

--- upsort/external/ex.js
+++ upsort/external/ex.js
@@ -1,16 +1,2 @@
-class upsortGet1X {
 
-    static async get22() {
-        let fres;
-
-       const tres = await kwjss.asobf('/t/22/06/upsort/dat/server.php?getOne=1');
-       fres = tres;
-       fres['rjs'] = UtoLocF(tres['U']);               
-        
-        return fres;
-       
-    }
-    
-
-}
 
