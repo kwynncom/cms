afb6672bde73a922079f90c193ab3abf27f770b8
kwynncom <buessresume@gmail.com>
2022-08-20 02:37:16 -0400

    almost ready to send to server with changes

--- cookies/index.php
+++ cookies/index.php
@@ -14,6 +14,10 @@
 
 <script src='/opt/kwynn/js/utils.js'></script>
 <script>
+	
+	var GDOV;
+	
+	
 class getEmailCookie {
 	constructor() {
 		onDOMLoad(() => { kwjss.sobf('/t/7/12/email/cookieMgrKw', {}, this.onret, false); });
@@ -21,6 +25,7 @@ class getEmailCookie {
 	
 	onret(res) {
 		inht('out10', res);
+		GDOV = new changeCVs(); 
 	}
 	
 }
@@ -33,7 +38,36 @@ function togglev(showit) {
 
 class changeCVs {
 	constructor() {
+		this.ckcks = [];
+		this.setKeys();
+	}
+	
+	doit() {
 		checkNum();
+		this.do10();
+	}
+	
+	setKeys() {
+		const cks = qsa('[data-ckey]');
+		cks.forEach((e) => { this.ckcks.push(e);	});		
+	}
+	
+	do10() {
+		const ckks = [];
+		this.ckcks.forEach((e) => { 
+			if (e.checked) ckks.push(e.dataset.ckey);
+		});
+		
+		const arecv = ckks.length > 0;
+		const cv = arecv ? '' : 'nothing checked';
+		const col = arecv ? 'white' : 'lightSalmon';
+		
+		this.ckcks.forEach((e) => { 
+			e.setCustomValidity(cv);
+			e.parentNode.style.backgroundColor = col;
+		});	
+		
+		cl(ckks);
 	}
 }
 
@@ -54,5 +88,21 @@ function checkNum() {
 	<div><input type='checkbox' onclick='togglev(this.checked);' /> show values (Don't let bad guys see values!)</div>
 	
 	<div id='out10'></div>
+	
+	<div style='margin-top: 1ex;'>
+	<input type='number' style='font-family: monospace; width: 5em;' oninput='checkNum();' id='cexHours' /> <label>hours </label>
+	<button onclick='GDOV.doit();'>change expiration</button>
+</div>
+
+<p>Check the boxes associated with names and then set the number of hours until expiration.  Fractions are fine.  Note these special cases:</p>
+
+<ul>
+	<li>0 means the cookie lasts until the browser closes</li>
+	<li>negative numbers will expire / delete the cookie(s)</li>
+</ul>
+
+<p>The hours box must be set to a number / any number.  At least one cookie's box has to be checked.
+	
+</p>
 </body>
 </html>
--- cookies/server.php
+++ cookies/server.php
@@ -1,26 +1,13 @@
 <table style='margin-top: 2ex; '>
-	<thead>
-		<tr><th></th><th>name</th><th>value</th></tr>
-	</thead>
+
+	<thead><tr><th></th><th>name</th><th>value</th></tr></thead>
+
 	<tbody>
-	
 <?php foreach($_COOKIE as $k => $v) { ?>
 	<tr>
-		<td><input type='checkbox' data-ckey='<?php echo($k); ?>' /></td>
+		<td><span><input type='checkbox' data-ckey='<?php echo($k); ?>' /></span></td>
 		<td><?php echo($k); ?></td><td class='cookv' style='visibility: hidden'><?php echo($v); ?></td>
 	</tr>
 <?php } ?>
 	</tbody>
 </table>
-
-<div style='margin-top: 1ex;'>
-	<input type='number' style='font-family: monospace; width: 5em;' oninput='checkNum();' id='cexHours' /> <label>hours </label>
-	<button onclick='new changeCVs();'>change expiration</button>
-</div>
-
-<p>Check the boxes associated with names and then set the number of hours until expiration.  Fractions are fine.  Note these special cases:</p>
-
-<ul>
-	<li>0 means the cookie lasts until the browser closes</li>
-	<li>negative numbers will expire / delete the cookie(s)</li>
-</ul>
