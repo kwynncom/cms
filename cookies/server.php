7ce4c750a8bde01b47153bfac9c295bf2c84314b
kwynncom <buessresume@gmail.com>
2022-08-20 02:58:56 -0400

    almost ready to receive new expiration

--- cookies/server.php
+++ cookies/server.php
@@ -1,3 +1,15 @@
 <?php
 
+$ha = headers_list(); // indexed 0, 1, ...
+$sck =  'Set-Cookie: ';
+$sckl = strlen($sck);
+foreach($ha as $r) {
+	if (strpos($r, $sck) === false) continue;
+	$r = substr($r, $sckl);
+	preg_match('/[^;=]+/', $r, $mnm);
+	preg_match('/expires=([^;]+)/', $r, $mex);
+	continue;
+}
+
 require_once('frag10.php');
+
