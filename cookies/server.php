95b21db996200bc7cc5b579fb7610528d0e56018
kwynncom <buessresume@gmail.com>
2022-08-20 01:36:22 -0400

    table form

--- cookies/server.php
+++ cookies/server.php
@@ -1,20 +1,8 @@
-<?php
+<table>
 
-if (1) {
-	// header('Content-Type: application/json');
-	echo(json_encode($_COOKIE, JSON_PRETTY_PRINT));
-}
-else  if (1) {
-	// header('Content-Type: text/plain');
-	var_dump($_COOKIE);
-}
-if (0) {
-$ha = headers_list(); // indexed 0, 1, ...
-foreach($ha as $r) {
-	if (strpos($r, 'Set-Cookie: ' . '') !== 0) continue;
-	preg_match('/expires=([^;]+)/', $r, $ms);
-	// return self::send20(kwifs($ms, 1), $preEx, $r, kwifs($ms, 0));
-	$ignore = 1;
-}
-
-}
+<?php foreach($_COOKIE as $k => $v) { ?>
+	<tr>
+		<td><?php echo($k); ?></td><td><?php echo($v); ?></td>
+	</tr>
+<?php } ?>
+</table>
