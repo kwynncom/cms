a6a045c3423f10266d81ba6b4e5d0482b298e498
kwynncom <buessresume@gmail.com>
2023-02-16 01:21:50 -0500

    cleaning up upsort

--- upsort/external/getOnePage.php
+++ upsort/external/getOnePage.php
@@ -6,8 +6,24 @@
 
 <title>last site update</title>
 <script src='/opt/kwynn/js/utils.js'></script>
-<script src='/t/22/06/upsort/external/external.js'></script>
 <script>
+	
+	function kwGetLatestWWW(cb) { // 2023/02/16 - moved from external file
+    
+		try {
+
+			const p10 = kwjss.sobf('/t/22/06/upsort/dat/server.php?getOne=1');
+			const p20 = new Promise((resolve) => onDOMLoad(resolve));
+			Promise.all([p10, p20 ])
+				.then(([r, ignoreFromDOM]) => 
+				{ 
+					r['rjs'] = UtoLocF(r['U']);
+					cb(r);
+				});
+		} catch (ex) { cl(ex); }
+	} // func
+	
+	
     function onLatest(res) {
         inht('gopa23', res['rjs']);
     }
@@ -33,4 +49,3 @@
 	</div>
 </body>
 </html>
-
--- upsort/external/getOneSS.php
+++ upsort/external/getOneSS.php
@@ -2,3 +2,5 @@
 
 require_once(__DIR__ . '/../dat/vis.php');
 echo(fileVis::getOneInternal());
+
+// 2023/02/16 maybe getting rid of the test pages from months ago
