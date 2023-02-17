01a9ef09020f068d0f4ada40d1a8a765a2523b20
kwynncom <buessresume@gmail.com>
2023-02-16 20:14:09 -0500

    making home page part of this; creating home page services

---
+++ home/index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<title>Kwynn's website</title>
+
+<link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico" />
+
+<script src='/opt/kwynn/js/utils.js'></script>
+<script>
+<?php require_once(__DIR__ . '/t/22/06/upsort/external/getOne.js'); ?>
+</script>
+
+<style>
+
+h2	{font-size: 120%; margin-top: 1.2ex; margin-bottom: 0 }
+ul	{margin-top: 0.5ex}
+li   { margin-bottom: 1.2ex; }
+
+.c21-10 { display: inline-block; }
+p { margin-bottom: 1ex; margin-top: 0.8ex; }
+.novax { margin-top: 1.5ex; margin-bottom: 0.9ex; display: inline-block; }
+
+h3 { margin-top: 0.2ex; margin-bottom: 0.2ex;  }
+.prphs { margin-top: 1ex; margin-bottom: 1ex; font-size: 85%; }
+.wvh2 { font-size: 140%; margin-top: 1.0ex;  }
+.nycd { margin-top: 0ex; margin-right: 7ex;}
+.l2d  { 
+    margin-top: 1.0ex; 
+    display: flex;
+    flex-flow: row wrap;
+    align-content: flex-start;
+    align-items: flex-start;
+}
+.nyh2 { margin-top: 0ex; }
+.htvd { 
+    align-self: flex-end;
+    margin-bottom: -0.2ex;
+    margin-top: 3ex; 
+}
+.immavp { font-size: 119%; }
+.swappp  { margin-top: 2ex; }
+.weekfcp { display: inline-block; margin-left: 1.0ex; }
+
+h1 { font-size: 150%;
+     margin-top:    0.2ex; 
+     margin-bottom: 0.2ex; 
+}
+
+#weeksfc { font-family: Monaco, Consolas, 'Andale Mono', monospace; font-size: 115%; } 
+
+</style>
+ 
+</head>
+<body style='font-family: sans-serif;'>
+    
+<div>
+    <h1  style='display: inline; font-size: 120%; '>Kwynn's website 
+        <span style='font-size: 95%; font-weight: normal;'>on software, with topic scope creep.</span></h1>
+    <div class='upl10' style='display: inline; padding-left: 0ex; font-size: 110%;  '>
+        <span style='overflow-wrap: break-word; '>
+            <span style='font-size: 90% '>Site updated</span>
+				<span style='display: inline-block; width: 31ch; text-align: center; font-family: monospace; font-size: 120%; ' 
+					  ><?php 
+						require_once(__DIR__ . '/t/22/06/upsort/dat/vis.php'); 
+						
+						$lo = json_decode(fileVis::getOneInternal(), true);
+						echo($lo['dHussjs']); unset($lo);
+
+						?>
+
+				</span>
+
+				<a href='/t/22/06/upsort/' style='font-size: 90%; padding-left: 0ex; '>update log</a>
+        </span>
+    </div>
+</div>
+    
+<div>
+
+<section>
+<h2 class='c21-10'>software:</h2> <p class='c21-10'> <a href='/t/3/02/Buess_resume.html'>my resume</a> with contact info, 
+    <a href='/t/20/10/github_guide.html'>my Github</a> (software projects), and 
+    <a href='/t/7/01/article_index.html'>other technical stuff</a>, including my <a href='/t/7/11/blog.html'>tech blog</a></p>
+
+<p class='immavp'>
+    I am immediately available for software gigs / work (<a href='/t/20/10/availability.html'>availability updates</a>).
+</p>
+
+<section class='swappp'>
+<h3 class='c21-10'><a href='/t/22/09/apprentice_2022_09.html'>seeking software apprentices</a></h3> 
+<p class='c21-10'>almost no experience needed, and I don't care 
+    where you are</p>
+</section>
+</section>
+</div>
+   
+<section>
+    <h2 class='wvh2'>worldview</h2>
+    
+    <ul style='margin-top: 0ex; '>
+		<li style='margin-top: 0.0ex; margin-bottom: 0.5ex; list-style-type: "&#127479;&#127482;"; font-size: 110%; ' >
+			<a href='/t/23/01/go_Russia.html' style='padding-left: 1ex; '>Go Russia!</a> 
+			<span style='display: inline-block; font-size: 150%; padding-left: 0.3ex; margin-top: 0ex; '>&#127479;&#127482;</span>
+		</li>
+	    <li><a href='/t/21/12/worldview_series.html'>worldview article series</a></li>
+        <li><a href='/t/21/12/personal_blog.html'>personal blog</a></li>
+		<li>It's week <span id='weeksfc' ><?php 
+			require_once(__DIR__ . '/t/21/12/flatten.php');
+			$weeko = new weeksFlattenCurve();
+			echo($weeko->weekn); 
+			unset($weeko);
+		?></span> to flatten the curve.</li>
+    </ul>
+</section>
+
+<section class='prphs'>
+    <h2 style='display: inline; font-size: 140%'><a href='/t/6/07/ql/quick_links.html'>quick links</a> 
+        <span style='font-size: 80%; font-weight: normal'>(mostly specific to my use)</span>
+    </h2>
+</section>
+    
+<div class='l2d'>
+<div class='nycd'>
+    
+<h2 style='font-family: monospace; display: inline-block; font-size: 170%; font-weight: bold; 
+    border: solid; 
+    border-color: rgb(40,40,40);  
+    border-width: thick;
+padding-top   : 0.2ex; 
+padding-left  : 0.5ex; padding-right: 0.6ex; 
+padding-bottom: 0.6ex; 
+' ><!--
+	--><a href='t/0/fn/fn_calculator.html' style='text-decoration: none'><!--
+        --><span style='text-decoration: underline'>Numerology Calculator</span><br /><!--
+	--><!--
+	--><span style='color: blue  ; background: orange'  >5</span><!--
+	--><span style='color: yellow; background: violet'  >3</span><!--
+	--><span style='color: green ; background: red'     >4</span><!--
+	--><span style='color: blue  ; background: orange'  >5</span><!--
+	--><span style='color: gold  ; background: violet'  >9</span><!--
+	--><span style='color: indigo; background: orange'  >6</span><!--
+	--><span style='color: yellow; background: violet'  >3</span><!--
+	--><span style='color: indigo; background: orange'  >6</span><!--	
+	--><span style='color: violet; background: yellow'  >7</span><!--	
+	--><span style='color: violet; background: yellow'  >7</span><!--	
+	--><span style='color: white;  background: white'   >&nbsp;</span><!--	
+	--><span style='color: yellow; background: violet'  >3</span><!--
+	--><span style='color: red;    background: green'   >1</span><!--
+	--><span style='color: yellow; background: violet'  >3</span><!--	
+	--><span style='color: yellow; background: violet'  >3</span><!--
+	--><span style='color: yellow; background: violet'  >3</span><!--
+	--><span style='color: yellow; background: violet'  >3</span><!--
+	--><span style='color: red;    background: green'   >1</span><!--
+	--><span style='color: orange; background: blue'    >2</span><!--
+	--><span style='color: indigo; background: orange'  >6</span><!--	
+    --><span style='color: gold  ; background: violet'  >9</span><!--
+	--></a></h2>
+</div> <!-- numerology calc link -->
+
+<div class='htvd'>
+    <a href="https://validator.w3.org/check?uri=referer"><img
+        src="/t/5/02/html5_valid.jpg"
+        alt="HTML5 validation check" width="103" height="36" /></a>
+</div>
+    </div> <!-- last 2 -->
+    
+</body>
+</html>
